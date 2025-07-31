50356d7b72ddfb19a691c77ee7d1c627cbcdd890
SURAJ2200 <ujjawalmishra729@gmail.com>
2025-07-31 19:16:01 +0530

    Add root route to redirect / to /posts

--- index.js
+++ index.js
@@ -168,6 +168,9 @@ app.delete("/posts/:id",(req,res)=>{
   posts = posts.filter((p)=>id!==p.id)
   res.redirect("/posts")
 })
+app.get("/", (req, res) => {
+  res.redirect("/posts");  
+});
     
 
 
