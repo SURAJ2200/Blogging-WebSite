769c04bbb78b9f560248d5b25c3d07897038a783
surajmishra <132044782+SURAJ2200@users.noreply.github.com>
2025-08-06 11:27:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require("express");
 const app = express();
 app.use(express.json())
-const port = 4000;
+const port = process.env.PORT || 4000;
 const path = require("path");
 const methodOverride =require("method-override")
 const { v4: uuidv4 } = require('uuid');
